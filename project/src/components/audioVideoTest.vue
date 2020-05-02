fd32895c2723ec740fbb755de8879853293e8c51
Gopinath <48796140+GopinathAP55@users.noreply.github.com>
2020-05-02 20:05:24 +0530

    speed test changes

--- project/src/components/audioVideoTest.vue
+++ project/src/components/audioVideoTest.vue
@@ -14,18 +14,20 @@
     >{{micText}}</button>
     <br />
 
-    <!-- <div data-app id="dialogBox">
-      <v-dialog v-model="dialog" width="50%">
+    <div data-app id="dialogBox">
+      <v-dialog v-model="dialog" persistent width="50%">
         <v-card>
           <v-card-title class="headline"></v-card-title>
 
           <v-card-text>
             <h4>{{errorText}}</h4>
           </v-card-text>
-          <v-card-text v-show='internet'>
-              Please stand by
-              <v-progress-linear indeterminate color="white" class="mb-0"></v-progress-linear>
-            </v-card-text>
+          <v-card-text v-show="internet">
+            <span>Checking internet speed...</span>
+            <br />
+            {{internetSpeed}}
+            <v-progress-linear indeterminate color="white" class="mb-0"></v-progress-linear>
+          </v-card-text>
           <div id="main">
             <video v-show="start" id="videoTag" playsinline autoplay controls></video>
             <audio
@@ -43,22 +45,15 @@
             <v-btn v-show="start" color="green darken-1" text @click="checkWebcam">close video</v-btn>
             <v-btn v-show="audioStart" color="green darken-1" text @click="testAudio">close audio</v-btn>
             <v-btn v-if="errorText !== ''" color="green darken-1" text @click="dialog=false">close</v-btn>
+            <v-btn
+              v-if="internet"
+              color="green darken-1"
+              text
+              @click="dialog=false, internet=false, internetSpeed=''"
+            >close</v-btn>
           </v-card-actions>
         </v-card>
-      </v-dialog> -->
-<meter id="disk_c" value="value" min="0" max="10">2 out of 10</meter><br>
-
-      <div data-app>
-        <v-dialog v-model="dialog" hide-overlay persistent width="300">
-             <v-card-title class="headline">title</v-card-title>
-          <v-card color="primary" dark>
-            <v-card-text>
-              Please stand by
-              <v-progress-linear indeterminate color="white" class="mb-0"></v-progress-linear>
-            </v-card-text>
-          </v-card>
-        </v-dialog>
-      </div>
+      </v-dialog>
     </div>
   </div>
 </template>
@@ -68,6 +63,7 @@
 import p from "vue-p5";
 import $ from "jquery";
 import axios from "axios";
+
 
 export default {
   name: "AudioVideoTest",
@@ -79,36 +75,40 @@ export default {
     micText: "Start MIC test",
     errorText: "",
     internetSpeed: "",
-    dialog: false,
+    dialog: true,
     internet: false,
-    value:0
+    internetSpeed: ""
   }),
-   watch: {
-      dialog (val) {
-        if (!val) return
+  watch: {
+    internet(val) {
+      if (!val) return;
 
-        setTimeout(() => (this.dialog = false), 4000)
-      }
-    },
+      setTimeout(() => (this.internet = false), 114000);
+    }
+  },
+  mounted() {
+    console.log("started");
+    var scriptTag = document.createElement("script");
+    scriptTag.src =  "https://cdn.jsdelivr.net/npm/p5@1.0.0/lib/addons/p5.sound.min.js";
+    scriptTag.id = "my-datatable";
+    document.getElementsByTagName("head")[0].appendChild(scriptTag);
+
+    // const script = function(p5) {
+    //   window.myp5 = p5;
+    //   let mic;
+    //   // NOTE: Set up is here
+    //   p5.setup = _ => {
+    //     mic = new myp5.AudioIn();
+    //   }; // NOTE: Draw is here
+    //   p5.draw = _ => {};
+    // }; // NOTE: Use p5 as an instance mode
+    // const P5 = require("p5");
+    // const P5Sound = require("p5/lib/addons/p5.sound.js");
+    // console.log(P5Sound);
+    // script(P5Sound);
+    // new P5(script);
+  },
   methods: {
-   
-    mounted() {
-      const script = function(p5) {
-        window.myp5 = p5;
-        let mic;
-        // NOTE: Set up is here
-        p5.setup = _ => {
-          mic = new myp5.AudioIn();
-        }; // NOTE: Draw is here
-        p5.draw = _ => {};
-      }; // NOTE: Use p5 as an instance mode
-      const P5 = require("p5");
-      const P5Sound = require("p5/lib/addons/p5.sound.js");
-      console.log(P5Sound);
-      script(P5Sound);
-      new P5(script);
-    },
-
     testAudio() {
       this.errorText = "";
       this.dialog = true;
@@ -134,6 +134,7 @@ export default {
           this.audioStart = false;
         }
       } else {
+        dialogBox.style.visibility = "visible";
         this.errorText = "Please check your internet connection.";
       }
     },
@@ -187,34 +188,49 @@ export default {
       }
     },
     checkInternetSpeed() {
+      this.dialog = true;
 
-      this.value =  Math.floor(Math.random() * (10 - 0) + 0); 
-       this.dialog = true;
-
-      const dialogBox = document.getElementById("dialogBox");
       this.internet = true;
       this.errorText = "";
-      console.log('into()');
+      const dialogBox = document.getElementById("dialogBox");
+      dialogBox.style.visibility = "visible";
+
+      console.log("into()");
       if (navigator.onLine) {
         let apiBaseUrl = "http://localhost:3000/api";
         axios
           .get(apiBaseUrl + "/speed")
           .then(res => {
-             console.log('into');
+            console.log("into");
             console.log(res);
-             dialogBox.style.visibility = "visible";
+            this.internetSpeed = res.data;
+            dialogBox.style.visibility = "visible";
           })
           .catch(e => {
             console.log(e);
           });
       } else {
-        this.errorText = "Please check your internet connection.";
+        dialogBox.style.visibility = "visible";
+        this.errorText = "Please check your internet connection yoyo.";
       }
     },
 
     checkMic() {
+      console.log(p)
       this.errorText = "";
-      this.mounted();
+     const script = function(p5) {
+      window.myp5 = p5;
+      let mic;
+      // NOTE: Set up is here
+      p5.setup = _ => {
+        mic = new p5.AudioIn();
+      }; // NOTE: Draw is here
+      p5.draw = _ => {};
+    }; // NOTE: Use p5 as an instance mode
+    const P5 = require("p5");
+    
+    script(P5);
+    new P5(script)
     }
   }
 };
